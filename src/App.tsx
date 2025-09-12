30ddb0028085a03af4188bcaf6ac9a4092d2435f
Dichido <220124673+DichDo@users.noreply.github.com>
2025-09-11 22:30:24 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,3 +25,19 @@ const App: React.FC = () => (
 );
 
 export default App;
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
