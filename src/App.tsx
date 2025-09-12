0b20feb1d8f71a5d7f16412758467511306edd53
Dichido <220124673+DichDo@users.noreply.github.com>
2025-09-11 17:05:12 -0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>✅ Vairi — Trust Through Verified Identity</h1>
+    </div>
+  );
+}
+
+export default App;
