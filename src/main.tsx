df8313b010616d6b6dbc0b9251317faaf636a3fc
Dichido <220124673+DichDo@users.noreply.github.com>
2025-09-11 17:16:37 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
