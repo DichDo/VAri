6fd55e7f7fba718982f5360a8fd5de5efbb995cc
Dichido <220124673+DichDo@users.noreply.github.com>
2025-09-11 18:39:38 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './App';
 import './index.css';
 import { useUserStore } from './store/userStore';
 
-// Restore user session before rendering the app
 useUserStore.getState().restoreUserFromStorage();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
